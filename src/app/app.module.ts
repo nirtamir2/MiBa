3254c9e3d9977dcba2604cee9ba33a6ea7188010
Nir Tamir <nirtamir2@gmail.com>
2016-12-25 18:29:39 +0200

    add angular 2 material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import 'hammerjs';
+
+import { MaterialModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,6 +14,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    MaterialModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
